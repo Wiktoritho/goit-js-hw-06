f3a418145a2657d56e9d43844a2e24d4f557da5f
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-11-28 10:42:13 +0100

    Create task-08.js

---
+++ js/task-08.js
@@ -0,0 +1,20 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+    const formObj = {
+        Email: email.value,
+        Password: password.value,
+  }
+  console.log(formObj);
+  event.currentTarget.reset();
+}
