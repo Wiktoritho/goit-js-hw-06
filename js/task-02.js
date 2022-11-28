cd30e7d5d6e0f2bcc7587a94d0b041b8ef6af700
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-11-28 10:42:02 +0100

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,15 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const list = document.querySelector("#ingredients");
+for (let element of ingredients) {
+  const item = document.createElement("li");
+  item.textContent = element;
+  item.classList.add("item");
+  list.append(item);
+};
